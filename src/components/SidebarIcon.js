f1a4d96665699f23f5547302e5e1bbb6bdf7ce87
1ro8s <saki0220hiro@gmail.com>
2022-04-27 13:33:48 +0900

    SidebarIcon作成

---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Icon from "../images/icon.ong";
+import Icon from "../logo.svg"
+
+const SidebarIcon = () => {
+  return (
+    <div className="sidebar-icon">
+      <img src={Icon} />
+      <p>saki0220hiro@gmail.com</p>
+    </div>
+  );
+};
+export default SidebarIcon;
